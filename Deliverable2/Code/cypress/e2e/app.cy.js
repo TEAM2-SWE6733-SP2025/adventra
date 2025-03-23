6a4ba30878cf3f31ad71df768d1f738c7bbe94af
Louis Muhammad <louis.muhammad@gmail.com>
2025-03-23 15:50:42 -0400

    Added e2e test for signin

--- Deliverable2/Code/cypress/e2e/app.cy.js
+++ Deliverable2/Code/cypress/e2e/app.cy.js
@@ -1,15 +1,41 @@
 describe("Navigation", () => {
   it("should navigate to the about page", () => {
-    // Start from the index page
     cy.visit("http://localhost:3000/");
 
-    // Find a link with an href attribute containing "about" and click it
-    cy.get('a[href*="about"]').click();
+    const moveMouseSlowly = (startX, startY, endX, endY, steps, delay) => {
+      const deltaX = (endX - startX) / steps;
+      const deltaY = (endY - startY) / steps;
 
-    // The new url should include "/about"
+      for (let i = 0; i <= steps; i++) {
+        const currentX = startX + deltaX * i;
+        const currentY = startY + deltaY * i;
+
+        cy.wait(delay);
+        cy.get("body").trigger("mousemove", {
+          clientX: currentX,
+          clientY: currentY,
+        });
+      }
+    };
+
+    moveMouseSlowly(500, 500, 500, 0, 20, 100); // 20 steps, 100ms delay between steps
+
+    cy.wait(1000);
+
+    cy.get("nav a[href='/about']").then(($links) => {
+      cy.log(`Found ${$links.length} matching elements`);
+    });
+
+    cy.get("nav a[href='/about']")
+      .first()
+      .should("exist")
+      .should("be.visible")
+      .click();
+
     cy.url().should("include", "/about");
 
-    // The new page should contain an h1 with "About"
     cy.get("h1").contains("About");
+
+    cy.wait(2000);
   });
 });
