49cf4b02a22bc8ee39c9a41e96bd5b86db183dee
Chris-Wilder <92411021+Chris-Wilder@users.noreply.github.com>
2025-03-23 14:02:19 -0400

    Added further rendering tests
    Couldn't get Prisma tests working, these are filler tests and can be updated once dynamic adventures are added. Prisma tests to come soon.

--- Deliverable2/Code/__tests__/page.test.js
+++ Deliverable2/Code/__tests__/page.test.js
@@ -27,4 +27,49 @@ describe("Home component", () => {
 
     expect(mainHeading).toBeInTheDocument();
   });
+
+  test("renders adventure names", () => {
+    const mockSession = {
+      user: {
+        name: "Test User",
+        email: "test@example.com",
+        image: "https://example.com/avatar.png",
+      },
+      expires: "2025-12-31T23:59:59.999Z",
+    };
+
+    render(
+      <SessionProvider session={mockSession}>
+        <Home />
+      </SessionProvider>,
+    );
+
+    const adventureName = screen.getByRole("heading", {
+      level: 2,
+      name: /Scuba Diving/i,
+    });
+
+    expect(adventureName).toBeInTheDocument();
+  });
+
+  test("renders adventure locations", () => {
+    const mockSession = {
+      user: {
+        name: "Test User",
+        email: "test@example.com",
+        image: "https://example.com/avatar.png",
+      },
+      expires: "2025-12-31T23:59:59.999Z",
+    };
+
+    render(
+      <SessionProvider session={mockSession}>
+        <Home />
+      </SessionProvider>,
+    );
+
+    const adventureLocation = screen.getByText(/Redang Island, Malaysia/i);
+
+    expect(adventureLocation).toBeInTheDocument();
+  });
 });
